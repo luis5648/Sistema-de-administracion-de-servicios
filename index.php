ff6173d0c032f5ee444602fbae602ac165665f7d
Luis <porta5648@gmail.com>
2019-06-24 15:13:49 -0500

    archivos del proyecto

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Página principal</title>
+</head>
+
+<body>
+
+    <div class="form-control">
+    </div>
+    <form action="" method="post">
+        <label for="nombre">Nombre: </label>
+        <input type="text" name="nombre" id="nombre">
+        <br>
+        <label for="codigo">Código: </label>
+        <input type="text" name="codigo" id="codigo">
+        <br><br>
+        <label for="categoria">Categoría: </label>
+        <select name="categoria" id="categoria">
+            <option value="instalacion de office">instalacion de office</option>
+            <option value="instalacion de windows">instalacion de windows</option>
+            <option value="falla en hardware">falla en hardware</option>
+            <option value="instalación de impresoras">instalación de impresoras</option>
+        </select>
+
+
+
+    </form>
+</body>
+</html>
